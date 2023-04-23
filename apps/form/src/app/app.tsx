ccf541cc8abb714c66e4aada8c7dd840823e446a
miuractech <miuractech@gmail.com>
2023-04-23 16:39:13 +0530

    v1

--- apps/form/src/app/app.tsx
+++ apps/form/src/app/app.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from "./app.module.css";
-import Form from "../data/1";
+import Form from "../data/data1";
 
 export function App() {
   return (
